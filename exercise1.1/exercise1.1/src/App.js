8513a029f92cef66893cf31f6167c8771afdfee5
Red <crabmarket56@gmail.com>
2023-02-01 16:49:02 +0900

    Create Header and Content components

--- exercise1.1/exercise1.1/src/App.js
+++ exercise1.1/exercise1.1/src/App.js
@@ -1,3 +1,27 @@
+const Header = (props) => {
+  return (
+    <>
+    {props.course}
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      Part One: {props.part1} <br></br>
+      Exercises:{props.exercises1}
+      <br></br>
+      Part Two: {props.part2} <br></br>
+      Exercises: {props.exercises2}
+      <br></br>
+      Part Three: {props.part3} <br></br>
+      Exercises: {props.exercises3}
+      <br></br>
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,16 +33,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course}/>
+      <Content part1={part1} exercises1= {exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
--- exercise1.1/exercise1.1/src/index.js
+++ exercise1.1/exercise1.1/src/index.js
@@ -4,11 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
