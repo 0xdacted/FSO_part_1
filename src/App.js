7a6edb12bcde2034c4b3fbaa7c7686a15675803c
Red <crabmarket56@gmail.com>
2023-02-01 15:47:58 +0900

    Define new component 'Hello'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a + b)
+
   return (
   <div>
-    <p>Hello world</p>
+    <h1>Greetings</h1>
+    <Hello/>
   </div>
   )
 }
