6666d7f1e888cca9bad8c8919a16bbf727fa4d06
Red <crabmarket56@gmail.com>
2023-02-06 14:40:05 +0900

    Add additional display features

--- exercises1/exercise1.6/unicafe/src/App.js
+++ exercises1/exercise1.6/unicafe/src/App.js
@@ -30,6 +30,10 @@ const Display = ({text, count}) => {
     const handleBad = () => {
       setBad(bad + 1)
     }
+
+    const total = good + bad + neutral
+    const average = (good - bad) / total
+    const positive = good / total
     return (
       <div>
       <h1>
@@ -47,6 +51,9 @@ const Display = ({text, count}) => {
         <Display text='good' count={good}/>
         <Display text='neutral' count={neutral}/>
         <Display text='bad' count={bad}/>
+        <Display text='all' count={total}/>
+        <Display text='average' count={average} />
+        <Display text='positive' count={positive}/>
       </div>
       </div>
     )
